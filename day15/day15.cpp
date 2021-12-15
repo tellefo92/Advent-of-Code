76b83867979488e18fd4bdf4c024bac71dc9868a
Tellef Østereng <tellefos@student.matnat.uio.no>
2021-12-15 08:25:07 +0100

    Solved part 1

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <map>
 
 using namespace std;
 
-int first() {
-    ifstream infile("day15.txt");
-    return 0;
+int Below(int i) {
+    return i + 100 <= 9999 ? i + 100 : -1;
 }
 
-int second() {
-    ifstream infile("day15.txt");
-    return 0;
+int Right(int i) {
+    return i % 100 < 99 ? i + 1 : -1;
+}
+
+void findAllPaths(vector<int>& cave, int i, int risk, vector<int>& risks, map<int,int>& visited) {
+    risk += cave[i];
+    map<int,int>::iterator it = visited.find(i);
+    if (it != visited.end()) {
+        if (risk >= visited[i]) {
+            return;
+        }
+    }
+    visited[i] = risk;
+    if (Below(i) == -1 && Right(i) == -1) {
+        risks.push_back(risk);
+        return;
+    }
+    if (Below(i) != -1) {
+        findAllPaths(cave, Below(i), risk, risks, visited);
+    } 
+    if (Right(i) != -1) {
+        findAllPaths(cave, Right(i), risk, risks, visited);
+    }
+}
+
+void min_risk(vector<int> cave) {
+    vector<int> risks;
+    int i = 0;
+    int risk = 0;
+    map<int,int> visited;
+    findAllPaths(cave, i, risk, risks, visited);
+    sort(risks.begin(), risks.end());
+    int ans = risks[0] - cave[0];
+    cout << ans << endl;
 }
 
 int main() {
-    cout << first() << endl;
-    cout << second() << endl;
+    ifstream infile("day15.txt");
+    string line;
+    vector<int> cave;
+    while(infile >> line) {
+        for (auto& c: line) {
+            cave.push_back(c-'0');
+        }
+    }
+    min_risk(cave);
+    return 0;
 }
