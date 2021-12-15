c0611db4822c1ad3a901bcb027c6595b61318815
Tellef Østereng <tellefos@student.matnat.uio.no>
2021-12-15 10:31:27 +0100

    Wrong answer on p2 despite test input working on both parts

--- day15/day15.cpp
+++ day15/day15.cpp
@@ -3,18 +3,23 @@
 #include <vector>
 #include <bits/stdc++.h>
 #include <map>
+#include <chrono>
+#include <math.h>
 
 using namespace std;
 
-int Below(int i) {
-    return i + 100 <= 9999 ? i + 100 : -1;
+int Below(int i, vector<int>& cave) {
+    int width = sqrt(cave.size());
+    return i + width < cave.size() ? i + width : -1;
 }
 
-int Right(int i) {
-    return i % 100 < 99 ? i + 1 : -1;
+int Right(int i, vector<int>& cave) {
+    int width = sqrt(cave.size());
+    return i % width < width - 1 ? i + 1 : -1;
 }
 
-void findAllPaths(vector<int>& cave, int i, int risk, vector<int>& risks, map<int,int>& visited) {
+
+void findAllPaths(vector<int>& cave, int i, int risk, int &lowest_risk, map<int,int>& visited) {
     risk += cave[i];
     map<int,int>::iterator it = visited.find(i);
     if (it != visited.end()) {
@@ -22,28 +27,58 @@ void findAllPaths(vector<int>& cave, int i, int risk, vector<int>& risks, map<in
             return;
         }
     }
-    visited[i] = risk;
-    if (Below(i) == -1 && Right(i) == -1) {
-        risks.push_back(risk);
+    if (risk >= lowest_risk) {
         return;
     }
-    if (Below(i) != -1) {
-        findAllPaths(cave, Below(i), risk, risks, visited);
+    visited[i] = risk;
+    if (Below(i, cave) == -1 && Right(i, cave) == -1) {
+        lowest_risk = risk;
+        return;
+    }
+    if (Below(i, cave) != -1) {
+        findAllPaths(cave, Below(i, cave), risk, lowest_risk, visited);
     } 
-    if (Right(i) != -1) {
-        findAllPaths(cave, Right(i), risk, risks, visited);
+    if (Right(i, cave) != -1) {
+        findAllPaths(cave, Right(i, cave), risk, lowest_risk, visited);
     }
 }
 
-void min_risk(vector<int> cave) {
-    vector<int> risks;
+int min_risk(vector<int>& cave) {
+    int lowest_risk = cave.size();
     int i = 0;
     int risk = 0;
     map<int,int> visited;
-    findAllPaths(cave, i, risk, risks, visited);
-    sort(risks.begin(), risks.end());
-    int ans = risks[0] - cave[0];
-    cout << ans << endl;
+    findAllPaths(cave, i, risk, lowest_risk, visited);
+    int ans = lowest_risk - cave[0];
+    return ans;
+}
+
+vector<int> scale_up(vector<int>& cave) {
+    vector<int> scaled_cave;
+    int width = sqrt(cave.size());
+    int height = sqrt(cave.size());
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k < width; k++) {
+                if (cave[k + i * height] + j > 9) {
+                    scaled_cave.push_back((cave[k + i * height] + j) % 9);
+                } else {
+                    scaled_cave.push_back(cave[k + i * height] + j);
+                }
+            }
+        }
+    }
+    int new_size = scaled_cave.size();
+    for (int i = 1; i < 5; i++) {
+        for (int j = 0; j < new_size; j++) {
+            if (scaled_cave[j] + i > 9) {
+                scaled_cave.push_back((scaled_cave[j] + i) % 9);
+            } else {
+                scaled_cave.push_back(scaled_cave[j] + i);
+            }
+        }
+    }
+    return scaled_cave;
 }
 
 int main() {
@@ -55,6 +90,17 @@ int main() {
             cave.push_back(c-'0');
         }
     }
-    min_risk(cave);
+    auto start = chrono::steady_clock::now();
+    int ans = min_risk(cave);
+    auto end = chrono::steady_clock::now();
+    chrono::duration<double> t = end - start;
+    cout << "Part 1: " << ans << ", solve time: " << t.count() << " s" << endl;
+    start = chrono::steady_clock::now();
+    vector<int> scaled_cave = scale_up(cave);
+    ans = min_risk(scaled_cave);
+    end = chrono::steady_clock::now();
+    t = end - start;
+    cout << "Part 2: " << ans << ", solve time: " << t.count() << " s" << endl;
+    // Not 2967
     return 0;
 }
